0eec46fc1943740da92589df390ecd179639ef60
Mohammad Anas <ansarianas53665@gmail.com>
2026-02-17 22:27:50 +0530

    initial commit

---
+++ part2/primitives.js
@@ -0,0 +1,25 @@
+//Number
+
+let balance = 120
+let anotherBalance = new Number(120)
+
+console.log(typeof balance);
+console.log(anotherBalance.valueOf());
+
+
+// null and undefined
+
+let firstName = null;
+let lastName = undefined
+console.log(firstName)
+console.log(lastName)
+
+let myString = "hello"
+let myStringOne = 'Hola'
+let userName = 'anas'
+
+let oldGreet = myString + " " + "anas"
+console.log(oldGreet)
+
+let greetMessage = `Hello ${userName}`
+console.log(greetMessage)

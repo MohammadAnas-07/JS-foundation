8bfaf6c9c505b4102e00008cbc9fb9c93208b210
Mohammad Anas <ansarianas53665@gmail.com>
2026-02-17 23:00:29 +0530

    Initial commit

---
+++ part2/nonPrimitives.js
@@ -0,0 +1,15 @@
+const userName = {
+  firstName: "Mohammad",
+  isLoggedin : true,
+}
+
+userName.firstName = "Mr.M"
+userName.lastName = "Anas"
+
+/* console.log(userName.firstName)
+console.log(userName.lastName)
+console.log(userName)
+console.log(typeof userName) */
+
+let today = new Date()
+console.log(today.getDate())

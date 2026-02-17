615418d1895564617b1792ee082c227252ffeeac
Mohammad Anas <ansarianas53665@gmail.com>
2026-02-17 21:37:25 +0530

    Initial commit

--- part1/logical.js
+++ part1/logical.js
@@ -5,4 +5,9 @@
 let isLoggedin = true
 let isPaid = true
 
-console.log(isLoggedin && isPaid)
+/* console.log(isLoggedin && isPaid) */
+
+let isEmailUser = true
+let isGoogleUser = false
+
+console.log(isEmailUser || isGoogleUser)

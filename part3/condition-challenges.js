f5265f7cc88d26c724d0d111752925ffeb0e2fb5
Mohammad Anas <ansarianas53665@gmail.com>
2026-02-19 10:46:57 +0530

    condition-challenges

---
+++ part3/condition-challenges.js
@@ -0,0 +1,53 @@
+// Checking if a number is greater than another
+
+/* let num1 = 5
+let num2 = 8
+console.log("I am regular upper code")
+
+
+if(num1 > num2){
+  console.log("num 1 is greater than num2")
+}else{
+  console.log("Nope num1 is NOT greater")
+}
+
+console.log("I am regular bottom code") */
+
+/* let userName = "anas"
+let anotherUserName  = "anas"
+
+if((userName == anotherUserName)){
+  console.log("Pick another username")
+} */
+
+// Checking if a number is a variable
+/* 
+  let score = "44"
+
+  if(typeof score === "number"){
+    console.log("Yep, this is a number")
+  }else{
+    console.log("No that is not a number")
+  } */
+
+//Checking if boolean value is true or false 
+
+/* let isReady = false
+
+if(isReady){
+  console.log("Tea is ready")
+}else{
+  console.log("Tea is NOT ready")
+} */
+
+//Checking if an array is empty or not:
+
+let items = [0]
+
+console.log(items.length)
+
+if(items.length === 0){
+  console.log("Array is empty")
+}else{
+  console.log("Array is NOT empty")
+}

eb1b02a1b1a0ae4399596c102ac7e5174c007815
Mohammad Anas <ansarianas53665@gmail.com>
2026-02-19 17:17:56 +0530

    loopChallenges

--- part4/loopChallenges.js
+++ part4/loopChallenges.js
@@ -8,3 +8,52 @@ while(i<=5){
   i++
 }
 console.log(sum) */
+
+/* 2. Write a `while` loop that counts down from 5 to 1 and stores the number in an array named `countdown` */
+
+/* let countdwon = []
+let j = 5;
+while(j > 0){
+  countdwon.push(j)
+  j--
+}
+console.log(countdwon) */
+
+/* 3. Write a `do while loop` that prompts a user to enter thier favorite tea type until they enter "stop".
+Store each tea type in an array named `teaCollection` */
+
+/* let teaCollection = []
+let tea
+do {
+  tea = prompt(`Enter your favourite tea (type "stop" to finish)`)
+  if(tea !== "stop"){
+    teaCollection.push(tea)
+  }
+} while (tea !== "stop");
+ */
+
+ 
+/* 4. Write a `do while` loop that adds numbers from 1 to 3
+and stores the result in a variable named `total` */
+
+/* let total = 0
+let k = 1
+do {
+  total +=k
+  k++
+} while (k <=3);
+console.log(total) */
+
+/* 5. Write a `for` loop that multiplies each element in the array `[2,4,6]` by 2 and stores the results in a new array named `multipliedNumbers`. */
+
+
+let multiliedNumber = []
+let numbers = [2,4,6]
+
+for (let l = 0; l < numbers.length; l++){
+  /* takeNumber = numbers[l] * 2
+  multiliedNumber.push(takeNumber) */
+
+  multiliedNumber.push(numbers[l] * 2)
+}
+console.log(multiliedNumber)

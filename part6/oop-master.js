4ad1afd6d6748b710aac7ba08a02a4d53b545516
Mohammad Anas <ansarianas53665@gmail.com>
2026-02-27 23:55:12 +0530

    Polymorphism

--- part6/oop-master.js
+++ part6/oop-master.js
@@ -98,4 +98,40 @@ let myMachine = new CoffeMachine()
 /* console.log(myMachine.start())
 console.log(myMachine.brewCoffee())
  */
-console.log(myMachine.pressStartButton())
+/* console.log(myMachine.pressStartButton()) */
+
+
+// Polymorphism
+
+class Bird{
+  fly(){
+    return `Flying....`
+  }
+}
+
+class Penguin extends Bird{
+  fly(){
+    return `Penguin can't fly`
+  }
+}
+
+let bird = new Bird()
+let penguin = new Penguin()
+/* console.log(bird.fly())
+console.log(penguin.fly())
+ */
+
+// static method
+
+class Calculator{
+   static add(a,b){
+    return a + b;
+  }
+}
+/* let miniCalc = new Calculator()
+console.log(miniCalc.add(2,3)) */
+
+/* console.log(Calculator.add(2,3)) */
+
+// Getters and setters
+

d0bbd4284b6db1ead4b30ae1e6375f41b3a4389d
Mohammad Anas <ansarianas53665@gmail.com>
2026-02-28 00:02:31 +0530

    get and set

--- part6/oop-master.js
+++ part6/oop-master.js
@@ -135,3 +135,29 @@ console.log(miniCalc.add(2,3)) */
 
 // Getters and setters
 
+class Employee{
+  #salary
+
+  constructor(name,salary){
+    if(salary < 0){
+      throw new Error("Salary cannot be in negative")
+    }
+    this.name = name
+    this.#salary = salary
+  }
+  
+  get salary(){
+    return `You are not allowed to see salary`
+  }
+
+  set salary(value){
+    if(value < 0){
+      console.error("Invalid Salary")
+    }else{
+      this._salary = value
+    }
+  }
+}
+let emp = new Employee("Alice",-50000)
+console.log(emp._salary)
+emp.salary = 60000

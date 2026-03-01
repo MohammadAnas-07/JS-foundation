e1b6b68ef01f5e2bc6eb015483e6e486d60524dd
Mohammad Anas <ansarianas53665@gmail.com>
2026-03-01 22:37:17 +0530

    adv js

---
+++ part9_adv/asyncAwait2.js
@@ -0,0 +1,32 @@
+function fetchPostData(){
+  return new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve("Post Data fetched")
+    },2000)
+  })
+}
+
+function fetchCommentData(){
+   return new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve("Commentd data fetched")
+    },3000)
+  })
+}
+
+async function getBlogData(){
+  try {
+    console.log("Fetching blog data")
+   /*  const blogData = await fetchPostData()
+    const commentData = await fetchCommentData() */
+   const [postData,commentData] = await Promise.all([fetchPostData(), fetchCommentData()])
+
+    console.log(postData)
+    console.log(commentData)
+
+    console.log("fetch complete")
+  } catch (error) {
+    console.log("Error fetching blog data" , error) 
+  }
+}
+getBlogData()
